56e17956168c6ce49a73c6665ad3d85db0777ee7
StanislaoEsposito <stanislaoesposito25@gmail.com>
2026-07-11 13:53:32 +0200

    Prima versione completa di Castrimaris

--- components/admin/SimpleEditor.tsx
+++ components/admin/SimpleEditor.tsx
@@ -106,7 +106,7 @@ export default function SimpleEditor({
   // Sincronizza il contenuto iniziale se cambia dall'esterno
   useEffect(() => {
     if (editor && initialContent && editor.getHTML() !== initialContent) {
-      editor.commands.setContent(initialContent, false);
+      editor.commands.setContent(initialContent, { emitUpdate: false });
     }
   }, [initialContent, editor]);
 
